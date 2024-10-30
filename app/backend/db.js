7771182c7efd22de0e10875dae17faf61aa39618
Meryem2510 <meryem18elfelloussi@gmail.com>
2024-10-30 23:35:20 +0100

    almost done

--- app/backend/db.js
+++ app/backend/db.js
@@ -6,7 +6,7 @@ const db = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE || 'express', // Nom de la base de données par défaut
+    database: process.env.DB_DATABASE,
 });
 
 db.connect((err) => {
